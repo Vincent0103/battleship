12de6e0631182e88ea24c1e893de982acfbc1b8d
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-12-26 17:34:11 +0100

    Add receiveAttack function that treats the players attacks and add its tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,104 +2,128 @@ import Ship from './ship.js';
 
 const Gameboard = () => {
   const partnerShips = []; const opponentShips = [];
-  const grid = []; const opponentsGrid = [];
-  // const missedShotsCoordinates = [];
+  const grid = []; const opponentGrid = [];
+  const missedShotsCoordinates = [];
 
   const buildGrids = () => {
     for (let i = 0; i < 10; i += 1) {
-      [grid, opponentsGrid].forEach((item) => item.push([]));
+      [grid, opponentGrid].forEach((item) => item.push([]));
       for (let j = 0; j < 10; j += 1) {
-        [grid, opponentsGrid].forEach((item, id) => item[i].push(
-          { hasShip: false, playerId: id },
+        [grid, opponentGrid].forEach((item, id) => item[i].push(
+          { shipId: false, playerId: id },
         ));
       }
     }
 
-    return { grid, opponentsGrid };
+    return { grid, opponentGrid };
   };
 
-  // const getShipFromCell = (x, y, ofPlayerId) => {
-  //   let targetId;
-  //   let ship;
-  //   if (ofPlayerId === 1) {
-  //     targetId = opponentsGrid[x][y].hasShip;
-  //     ship = opponentShips.forEach((opponentShip) => {
-  //       if (opponentShip.getId() === targetId) return targetId;
-  //       throw new Error('Couldn\'t find the ship');
-  //     });
-  //   } else {
-  //     targetId = grid[x][y].hasShip;
-  //     ship = partnerShips.forEach((partnerShip) => {
-  //       if (partnerShip.getId() === targetId) return targetId;
-  //       throw new Error('Couldn\'t find the ship');
-  //     });
-  //   }
-  //   return ship;
-  // };
+  const getShipFromCell = (x, y, ofPlayerId) => {
+    const [currentGrid, ships] = (ofPlayerId === 1)
+      ? [opponentGrid, opponentShips] : [grid, partnerShips];
+    const targetId = currentGrid[x][y].shipId;
+    const [shipRetrieved] = ships.filter((opponentShip) => opponentShip.getId() === targetId);
+    if (!shipRetrieved) throw new Error('Couldn\'t find the ship');
+    return shipRetrieved;
+  };
 
-  const occupyCells = ({ getLength, getId }, [y, x], orientation) => {
+  const occupyCells = ({ getLength, getId }, [y, x], ofPlayerId, orientation) => {
+    const currentGrid = (ofPlayerId === 0) ? grid : opponentGrid;
     if (orientation === 'rightward') {
       for (let i = 0; i < getLength(); i += 1) {
-        grid[y][x + i].hasShip = getId();
+        currentGrid[y][x + i].shipId = getId();
       }
     } else {
       for (let j = 0; j < getLength(); j += 1) {
-        grid[y + j][x].hasShip = getId();
+        currentGrid[y + j][x].shipId = getId();
       }
     }
-    return grid;
+    return currentGrid;
   };
 
-  const isCellAvailable = ([y, x], isPlaceable = true) => {
+  const isCellAvailable = ([y, x], ofPlayerId, isPlaceable = true) => {
     let canBePlaced = isPlaceable;
-    if (grid[y] === undefined || grid[y][x] === undefined) return false;
-    if (grid[y][x].hasShip) canBePlaced = false;
+    const currentGrid = (ofPlayerId === 0) ? grid : opponentGrid;
+    if (currentGrid[y] === undefined || currentGrid[y][x] === undefined) return false;
+    if (currentGrid[y][x].shipId) canBePlaced = false;
     return canBePlaced;
   };
 
-  const isPlaceableSynchoronously = (length, [y, x], orientation, isPlaceable) => {
+  const isPlaceableSynchoronously = (length, [y, x], ofPlayerId, orientation, isPlaceable) => {
     let canBePlaced = isPlaceable;
     if (orientation === 'rightward') {
       for (let i = 0; i < length; i += 1) {
-        canBePlaced = isCellAvailable([y, x + i], isPlaceable);
+        canBePlaced = isCellAvailable([y, x + i], ofPlayerId, isPlaceable);
       }
     } else {
       for (let j = 0; j < length; j += 1) {
-        canBePlaced = isCellAvailable([y + j, x], isPlaceable);
+        canBePlaced = isCellAvailable([y + j, x], ofPlayerId, isPlaceable);
       }
     }
 
     return canBePlaced;
   };
 
-  const areCellsAvailable = (length, coordinates, orientation) => {
+  const areCellsAvailable = (length, coordinates, ofPlayerId, orientation) => {
     let isPlaceable = true;
 
     // the for loop acts asynchoronously so we had to seperate it into it's own function
     // in order to make it synchoronous
-    isPlaceable = isPlaceableSynchoronously(length, coordinates, orientation, isPlaceable);
+    isPlaceable = isPlaceableSynchoronously(
+      length,
+      coordinates,
+      ofPlayerId,
+      orientation,
+      isPlaceable,
+    );
     return isPlaceable;
   };
 
-  const placeShip = (ship, coordinates, orientation = 'rightward') => {
-    if (areCellsAvailable(ship.getLength(), coordinates, orientation)) {
-      partnerShips.push(ship);
-      return occupyCells(ship, coordinates, orientation);
+  const placeShip = (ship, coordinates, ofPlayerId = 0, orientation = 'rightward') => {
+    if (areCellsAvailable(ship.getLength(), coordinates, ofPlayerId, orientation)) {
+      if (ofPlayerId === 0) partnerShips.push(ship);
+      else opponentShips.push(ship);
+      return occupyCells(ship, coordinates, ofPlayerId, orientation);
     }
     return false;
   };
 
-  // const receiveAttack = (coordinates) => {
-  //   const [x, y] = coordinates;
-  //   if (Number.isInteger(opponentsGrid[x][y].hasShip)) {
-  //     const ship = getShipFromCell(x, y, 1);
-  //     ship.hit();
-  //   } else {
+  const receiveAttack = (coordinates, ofPlayerId) => {
+    const [x, y] = coordinates;
+    if (ofPlayerId === 0 && Number.isInteger(opponentGrid[x][y].shipId)) {
+      const ship = getShipFromCell(x, y, 1);
+      ship.hit();
+      return ship;
+    } if (ofPlayerId === 1 && Number.isInteger(grid[x][y].shipId)) {
+      const ship = getShipFromCell(x, y, 0);
+      ship.hit();
+      return ship;
+    }
+    missedShotsCoordinates.push({ coordinates, ofPlayerId });
+    return missedShotsCoordinates;
+  };
 
-  //   }
-  // };
-
-  return { buildGrids, placeShip };
+  return { buildGrids, placeShip, receiveAttack };
 };
+
+// const gameboard2 = Gameboard();
+// gameboard2.buildGrids();
+
+// const ship1 = Ship(4);
+// const ship2 = Ship(6, 3);
+// const ship3 = Ship(2);
+// const ship4 = Ship(5);
+// const ship5 = Ship(1);
+// const ship6 = Ship(3, 2);
+// const ship7 = Ship(5);
+
+// gameboard2.placeShip(ship1, [0, 2], 1, 'downward');
+// gameboard2.placeShip(ship2, [3, 7], 1, 'downward');
+// gameboard2.placeShip(ship3, [1, 5], 1);
+// gameboard2.placeShip(ship4, [4, 1]);
+// gameboard2.placeShip(ship5, [5, 7], 0, 'downward');
+// gameboard2.placeShip(ship6, [9, 0], 1);
+// gameboard2.placeShip(ship7, [3, 4]);
+// console.log(gameboard2.receiveAttack([0, 2], 0));
 
 export default Gameboard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,7 +3,7 @@ import Ship from '../ship';
 
 describe('Gameboard', () => {
   let gameboard1; let gameboard2; let gameboard3; let gameboard4;
-  let ship1; let ship2; let ship3;
+  let ship1; let ship2; let ship3; let ship4; let ship5; let ship6; let ship7;
   let currentGridPartToTest;
   beforeEach(() => {
     gameboard1 = Gameboard();
@@ -16,16 +16,20 @@ describe('Gameboard', () => {
     ship1 = Ship(4);
     ship2 = Ship(6, 3);
     ship3 = Ship(2);
+    ship4 = Ship(5);
+    ship5 = Ship(1);
+    ship6 = Ship(3, 2);
+    ship7 = Ship(5);
   });
 
   test('grids built correctly', () => {
     const grids = gameboard1.buildGrids();
     grids.grid.forEach((line) => line.forEach((item) => {
-      expect(item.hasShip).toBe(false);
+      expect(item.shipId).toBe(false);
       expect(item.playerId).toEqual(0);
     }));
-    grids.opponentsGrid.forEach((line) => line.forEach((item) => {
-      expect(item.hasShip).toBe(false);
+    grids.opponentGrid.forEach((line) => line.forEach((item) => {
+      expect(item.shipId).toBe(false);
       expect(item.playerId).toEqual(1);
     }));
   });
@@ -33,25 +37,40 @@ describe('Gameboard', () => {
   test('place ship correctly in the grid map', () => {
     currentGridPartToTest = gameboard2.placeShip(ship1, [0, 2])[0].slice(2, 6);
     currentGridPartToTest.forEach((item) => {
-      expect(Number.isInteger(item.hasShip)).toBeTruthy();
+      expect(Number.isInteger(item.shipId)).toBeTruthy();
     });
 
-    currentGridPartToTest = gameboard3.placeShip(ship3, [6, 3], 'downward').slice(6, 8);
+    currentGridPartToTest = gameboard3.placeShip(ship3, [6, 3], 1, 'downward').slice(6, 8);
     currentGridPartToTest.forEach((item) => {
-      expect(Number.isInteger(item[3].hasShip)).toBeTruthy();
+      expect(Number.isInteger(item[3].shipId)).toBeTruthy();
     });
 
-    currentGridPartToTest = gameboard4.placeShip(ship2, [3, 8], 'downward').slice(3, 9);
+    currentGridPartToTest = gameboard4.placeShip(ship2, [3, 8], 0, 'downward').slice(3, 9);
     currentGridPartToTest.forEach((item) => {
-      expect(Number.isInteger(item[8].hasShip)).toBeTruthy();
+      expect(Number.isInteger(item[8].shipId)).toBeTruthy();
     });
   });
 
   test('ship can\'t be placed on an incorrect area of the grid', () => {
-    currentGridPartToTest = gameboard2.placeShip(ship2, [2, 8], 'rightward');
+    currentGridPartToTest = gameboard2.placeShip(ship2, [2, 8], 1, 'rightward');
     expect(currentGridPartToTest).toBe(false);
 
-    currentGridPartToTest = gameboard3.placeShip(ship3, [10, 1], 'downward');
+    currentGridPartToTest = gameboard3.placeShip(ship3, [10, 1], 0, 'downward');
     expect(currentGridPartToTest).toBe(false);
+  });
+
+  test('receive attack correctly when given valid coordinates', () => {
+    gameboard2.placeShip(ship1, [0, 2], 1, 'downward');
+    gameboard2.placeShip(ship2, [3, 7], 1, 'downward');
+    gameboard2.placeShip(ship3, [1, 5], 1);
+    gameboard2.placeShip(ship4, [4, 1]);
+    gameboard2.placeShip(ship5, [5, 7], 0, 'downward');
+    gameboard2.placeShip(ship6, [9, 0], 1);
+    gameboard2.placeShip(ship7, [3, 4]);
+    expect(gameboard2.receiveAttack([1, 2], 0).getId()).toEqual(ship1.getId());
+    expect(gameboard2.receiveAttack([4, 3], 1).getId()).toEqual(ship4.getId());
+    expect(typeof gameboard2.receiveAttack([0, 1], 0)).toEqual('object');
+    expect(typeof gameboard2.receiveAttack([5, 8], 1)).toEqual('object');
+    expect(gameboard2.receiveAttack([1, 6], 0).getId()).toEqual(ship3.getId());
   });
 });
