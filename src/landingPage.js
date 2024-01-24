51628d98b1c03a7e25da9d5f1e2583d3b807695d
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-01-24 21:38:31 +0100

    Add drag & drop user option for placeable ships

--- src/landingPage.js
+++ src/landingPage.js
@@ -6,6 +6,52 @@ const LandingPage = (landingPageContainer) => {
   let gridContainer = landingPageDIV.querySelector('.grid-container.user-placeable');
   const startBtn = landingPageDIV.querySelector('.start-btn');
 
+  function listenDragEnd() {
+    this.style.opacity = '1';
+  }
+
+  function listenDragOver(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  function listenDragEnter(square, shipLength) {
+    square.classList.toggle('over');
+    const line = square.parentElement;
+    const gridContainerX = parseInt(square.getAttribute('data-square'), 10);
+    for (let i = 1; i < shipLength; i += 1) {
+      const currentSquare = line.children[gridContainerX + i];
+      if (currentSquare === undefined) return false;
+      currentSquare.classList.toggle('over');
+    }
+  }
+
+  function listenDragLeave(square, shipLength) {
+    square.classList.toggle('over');
+    const line = square.parentElement;
+    const gridContainerX = parseInt(square.getAttribute('data-square'), 10);
+    for (let i = 1; i < shipLength; i += 1) {
+      const currentSquare = line.children[gridContainerX + i];
+      if (currentSquare === undefined) return false;
+      currentSquare.classList.toggle('over');
+    }
+  }
+
+  function listenDrop(e, square, shipLength) {
+    e.stopPropagation();
+    square.classList.remove('over');
+    square.classList.add('ship');
+    const line = square.parentElement;
+    const gridContainerX = parseInt(square.getAttribute('data-square'), 10);
+    for (let i = 1; i < shipLength; i += 1) {
+      const currentSquare = line.children[gridContainerX + i];
+      if (currentSquare === undefined) return false;
+      currentSquare.classList.remove('over');
+      currentSquare.classList.add('ship');
+    }
+    return false;
+  }
+
   const addShipCells = (shipContainer, shipLength) => {
     for (let i = 0; i < shipLength; i += 1) {
       const shipCell = document.createElement('div');
@@ -25,10 +71,30 @@ const LandingPage = (landingPageContainer) => {
     gridContainer = buildGrid(gridContainer);
     listenStartBtn();
     const shipLengths = [5, 4, 3, 3, 2];
+    let currentDraggedShipLength = 0;
 
     for (let i = 0; i < 5; i += 1) {
-      addShipCells(placeableShipsContainer.children[i], shipLengths[i]);
+      const currentShip = placeableShipsContainer.children[i];
+      currentShip.setAttribute('draggable', 'true');
+      addShipCells(currentShip, shipLengths[i]);
+
+      // eslint-disable-next-line no-loop-func
+      currentShip.addEventListener('dragstart', () => {
+        currentShip.style.opacity = '.4';
+        currentDraggedShipLength = currentShip.children.length;
+        console.log(currentDraggedShipLength);
+      });
+      currentShip.addEventListener('dragend', listenDragEnd);
     }
+
+    Array.from(gridContainer.children).forEach((line) => {
+      Array.from(line.children).forEach((square) => {
+        square.addEventListener('dragover', listenDragOver);
+        square.addEventListener('dragenter', () => listenDragEnter(square, currentDraggedShipLength));
+        square.addEventListener('dragleave', () => listenDragLeave(square, currentDraggedShipLength));
+        square.addEventListener('drop', (e) => listenDrop(e, square, currentDraggedShipLength));
+      });
+    });
   };
 
   addContent();
